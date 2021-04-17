5e29984a7e1e7dd8a5c6465ad5ea741f96e7e265
davidlasry <lasry.david@gmail.com>
2021-04-17 12:39:26 +0300

    remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,6 @@ export class AppComponent {
   }
 
   submit(): void {
-    // console.log(this.form.value);
-    // console.log(this.form.errors);
-    // console.log("form.valid", this.form.valid);
+    console.log(this.form.value);
   }
 }
